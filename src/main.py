915c86478b084e7929847040f1174554a109445f
mchin24 <mchin24@gmail.com>
2026-01-13 16:36:23 -0500

    Added TextNode class and TextType enum. Simple main.py to test.

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("Hello, World!", TextType.TEXT, 'http://example.com')
+    print(node)
+
+if __name__ == "__main__":
+    main()
