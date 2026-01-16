4106ae5d08074dccff49fa969f4fd82c9777f745
mchin24 <mchin24@gmail.com>
2026-01-16 09:32:47 -0500

    Updated gitignore, removed public dir

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from textnode import TextNode, TextType
+from utils import copy_static_files
+import os
 
 def main():
-    node = TextNode("Hello, World!", TextType.TEXT, 'http://example.com')
-    print(node)
+    src_directory = os.path.abspath('./static')
+    dest_directory = os.path.abspath('./public')
+    
+    copy_static_files(src_directory, dest_directory)
+    print(f"Static files copied from {src_directory} to {dest_directory}")
 
 if __name__ == "__main__":
     main()
